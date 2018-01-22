555987c93185df9f13b9d9c9292d7d4b04451fc5
xiaohaizhong <xiaohaizhong@skyunion.net>
2018-01-22 19:12:52 +0800

    这边在app模块内引入 具体模块就可以引入并使用本地存储

--- src/pages/todo/todo.ts
+++ src/pages/todo/todo.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { IonicPage, NavController, PopoverController, ModalController } from 'ionic-angular';
 
 /**
  * Generated class for the TodoPage page.
@@ -20,7 +21,10 @@ export class TodoPage {
   done: any;
   dones: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public modalCtrl: ModalController,
+              public popoverCtrl: PopoverController,
+              public storage: Storage) {
     this.todo = [];
     this.todos = [];
     this.done = [];
@@ -34,12 +38,15 @@ export class TodoPage {
   deleteEvent(data){
     
   }
+
   todoEvent(data){
     
   }
+
   doneEvent(data){
     
   }
+  
   openEvent(data){
 
   }
